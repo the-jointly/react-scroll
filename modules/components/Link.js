895ed5f6d0ad96819d5264f7fd2024dd8a9b1f68
Andres Garcia <spike886@gmail.com>
2020-10-01 12:19:58 -0300

    fix element not building

--- modules/components/Link.js
+++ modules/components/Link.js
@@ -1,10 +1,12 @@
 "use strict";
 
-import React from 'react';
-import ScrollLink from '../mixins/scroll-link';
+import React from "react";
+import ScrollLink from "../mixins/scroll-link";
 
 class LinkElement extends React.Component {
-  render = () => (<a {...this.props}>{this.props.children}</a>)
-};
+  render() {
+    return <a {...this.props}>{this.props.children}</a>;
+  }
+}
 
-export default ScrollLink(LinkElement)
+export default ScrollLink(LinkElement);
